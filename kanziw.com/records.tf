9a9cde3d3c42270eb6638bbd2e5a97b6476a10a8
Jiwoong Jung <kanziwoong@gmail.com>
2022-09-24 17:50:52 +0900

    Add terraform action (#4)

--- kanziw.com/records.tf
+++ kanziw.com/records.tf
@@ -4,7 +4,7 @@ resource "cloudflare_record" "terraform_managed_resource_430849162e814a764b9ab76
   ttl     = 1
   type    = "A"
   value   = "151.101.1.195"
-  zone_id = var.zone_id 
+  zone_id = var.zone_id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_89ee5a8b736235257c7e21f5476057ef" {
@@ -13,7 +13,7 @@ resource "cloudflare_record" "terraform_managed_resource_89ee5a8b736235257c7e21f
   ttl     = 1
   type    = "A"
   value   = "151.101.65.195"
-  zone_id = var.zone_id 
+  zone_id = var.zone_id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_2b561c6abf7a4acdad41374b641b1cd2" {
@@ -22,7 +22,7 @@ resource "cloudflare_record" "terraform_managed_resource_2b561c6abf7a4acdad41374
   ttl     = 1
   type    = "A"
   value   = "27.0.236.139"
-  zone_id = var.zone_id 
+  zone_id = var.zone_id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_c3138aa6a60de91a654aa09bc4822e9f" {
@@ -31,7 +31,7 @@ resource "cloudflare_record" "terraform_managed_resource_c3138aa6a60de91a654aa09
   ttl     = 1
   type    = "CNAME"
   value   = "host.tistory.io"
-  zone_id = var.zone_id 
+  zone_id = var.zone_id
 }
 
 resource "cloudflare_record" "terraform_managed_resource_5a248a7c905f5149ef9cfea335876b5b" {
@@ -40,5 +40,5 @@ resource "cloudflare_record" "terraform_managed_resource_5a248a7c905f5149ef9cfea
   ttl     = 1
   type    = "TXT"
   value   = "google-site-verification=2zTBaDkQXfK7mqxVurwaKt8gflX1VBdl4-XsDqRI5vE"
-  zone_id = var.zone_id 
+  zone_id = var.zone_id
 }
